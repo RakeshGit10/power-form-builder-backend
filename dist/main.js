144fdc8b8122ea07ec42ad82a78ce0c283c1aee3
Rakesh Pulli <rakesh.pulli@blueconchtech.com>
2023-02-22 15:53:53 +0530

    Added code for storing User information

--- dist/main.js
+++ dist/main.js
@@ -258,6 +258,56 @@ app.delete('/api/form/delete/:id', (req, res, next) => __awaiter(void 0, void 0,
     }
     res.status(200).json({ success: true });
 }));
+//
+const userSchema = new mongoose_1.default.Schema({
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    firstname: { type: String, required: true },
+    lastname: { type: String, required: true },
+});
+const User = mongoose_1.default.model("User", userSchema);
+app.post("/api/signup", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const { email, password, firstname, lastname } = req.body;
+        const user = yield User.findOne({ email });
+        if (user) {
+            return res.status(400).json({ message: "Email already registered" });
+        }
+        const newUser = new User({
+            email,
+            password,
+            firstname,
+            lastname,
+        });
+        yield newUser.save();
+        return res.status(200).json({ message: "User created successfully" });
+    }
+    catch (error) {
+        console.log(error);
+        return res.status(500).json({ message: "Server Error" });
+    }
+}));
+app.post('/api/signin', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { email, password } = req.body;
+    try {
+        // Find the user with the given email
+        const user = yield User.findOne({ email });
+        console.log(user);
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+        const isMatch = password === user.password;
+        if (!isMatch) {
+            return res.status(400).json({ message: 'Invalid password' });
+        }
+        const { _id, firstname, lastname, email: userEmail } = user;
+        return res.status(200).json({ _id, firstname, lastname, email: userEmail });
+    }
+    catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: 'Server error' });
+    }
+}));
 app.listen(4000, () => {
     console.log("On port 4000");
 });
--- main.ts
+++ main.ts
@@ -306,6 +306,69 @@ app.delete('/api/form/delete/:id', async (req: Request, res: Response, next: Nex
   res.status(200).json({success: true})
 })
 
+//
+const userSchema = new mongoose.Schema({
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  firstname: { type: String, required: true },
+  lastname: { type: String, required: true },
+});
+
+const User = mongoose.model("User", userSchema);
+
+app.post("/api/signup", async (req, res) => {
+  try {
+    const { email, password, firstname, lastname } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (user) {
+      return res.status(400).json({ message: "Email already registered" });
+    }
+
+    const newUser = new User({
+      email,
+      password,
+      firstname,
+      lastname,
+    });
+
+    await newUser.save();
+
+    return res.status(200).json({ message: "User created successfully" });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Server Error" });
+  }
+});
+
+
+app.post('/api/signin', async (req, res) => {
+  const { email, password } = req.body;
+  
+  try {
+    // Find the user with the given email
+    const user = await User.findOne({ email });
+    console.log(user)
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+  
+    const isMatch = password === user.password
+    if (!isMatch) {
+      return res.status(400).json({ message: 'Invalid password' });
+    }
+
+    const {_id, firstname, lastname, email: userEmail } = user;
+    return res.status(200).json({_id, firstname,lastname, email: userEmail });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: 'Server error' });
+  }
+});
+
+
+
 app.listen(4000, () => {
   console.log("On port 4000");
 });
